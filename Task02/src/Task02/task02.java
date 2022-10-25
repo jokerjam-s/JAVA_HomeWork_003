395858492fe51d14051a626b2565b39070adc4b9
Андрей <40392418+jokerjam-s@users.noreply.github.com>
2022-10-25 17:26:56 +0300

    work ready

--- Task02/src/Task02/task02.java
+++ Task02/src/Task02/task02.java
@@ -1,2 +1,62 @@
-package Task02;public class task02 {
+/*
+    Пусть дан произвольный список целых чисел, удалить из него четные числа.
+
+    Коичество элементов в списке запросим у пользователя, список заполним
+    случайными значениями.
+
+ */
+
+package Task02;
+
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+import java.util.logging.Logger;
+
+public class task02 {
+    // максимальная граница генерируемых значений
+    private static final int MAX_NUM = 100;
+
+
+    public static void main(String[] args) {
+        clearScreen();
+
+        Random random = new Random();
+        Logger logger = Logger.getAnonymousLogger();
+
+        int arraySize = readIntConsole("Размерность списка: ");
+        ArrayList<Integer> arrayList = new ArrayList<>(arraySize);
+
+        for (int i = 0; i < arraySize; i++) {
+            arrayList.add(random.nextInt(MAX_NUM));
+        }
+
+        logger.info(arrayList.toString());
+        logger.info(deleteEven(arrayList).toString());
+    }
+
+    // удаление четных элементов
+    private static ArrayList<Integer> deleteEven(ArrayList<Integer> array) {
+        for (int i = array.size() - 1; i >=0 ; i--) {
+            if((array.get(i) % 2) == 0){
+                array.remove(i);
+            }
+        }
+        return array;
+    }
+
+    // очистка терминала
+    public static void clearScreen() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
+
+    // получение целочисленного значения с консоли
+    public static int readIntConsole(String message) {
+        System.out.print(message);
+        Scanner scanner = new Scanner(System.in);
+
+        return scanner.nextInt();
+    }
+
 }
--- Task03/src/Task03/task03.java
+++ Task03/src/Task03/task03.java
@@ -1,2 +1,58 @@
-package Task03;public class task03 {
+/*
+    Задан целочисленный список ArrayList. Найти минимальное, максимальное и среднее из этого списка.
+
+    Коичество элементов в списке запросим у пользователя, список заполним
+    случайными значениями.
+ */
+
+package Task03;
+
+import java.util.*;
+import java.util.logging.Logger;
+
+public class task03 {
+    // максимальная граница генерируемых значений
+    private static final int MAX_NUM = 100;
+
+    public static void main(String[] args) {
+        clearScreen();
+
+        Random random = new Random();
+
+        // генерация, заполнение
+        int arraySize = readIntConsole("Размерность списка: ");
+        ArrayList<Integer> arrayList = new ArrayList<>(arraySize);
+        for (int i = 0; i < arraySize; i++) {
+            arrayList.add(random.nextInt(MAX_NUM));
+        }
+
+        // расчеты
+        int minVal = Collections.min(arrayList);
+        int maxVal = Collections.max(arrayList);
+        float avgVal = (float) (arrayList.stream().mapToDouble(Integer::floatValue).sum() / arrayList.size());
+
+        // вывод, логирование
+        Logger logger = Logger.getAnonymousLogger();
+        logger.info(arrayList.toString());
+
+        logger.info("Макс. значение  : " + maxVal);
+        logger.info("Мин. значение   : " + minVal);
+        logger.info("Среднее значение: " + avgVal);
+    }
+
+
+    // очистка терминала
+    public static void clearScreen() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
+
+    // получение целочисленного значения с консоли
+    public static int readIntConsole(String message) {
+        System.out.print(message);
+        Scanner scanner = new Scanner(System.in);
+
+        return scanner.nextInt();
+    }
+
 }
